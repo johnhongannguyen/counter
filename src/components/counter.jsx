77e597dd199dea2f61588d9d9d8962a197d92067
johnhongannguyen <johhaneshongan@gmail.com>
2021-03-26 10:40:25 -0700

    conditional rendering

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -4,41 +4,30 @@ class Counter extends Component {
     state = {
         count: 0,
         imageUrl: 'https://picsum.photos/200',
-        tags: ['tag1','tag2','tag3']
+        tags: []
 
     };
+    renderTags(){
+        if(this.state.tags.length === 0)
+        return <p>There is no tag !!!</p>;
 
-    styles = {
-        fontSize: 10,
-        fontWeight: 'bold'
+        return <ul>
+        {this.state.tags.map(tag => <li key={tag}>{tag}</li>)}
+            </ul>
+        
     }
-
-   render() { 
+    render() { 
       
 
         return(
-        <div> 
-            <img className="img img-thumbnail" src={this.state.imageUrl} alt=""/>
-            <span className={this.getBadgeClasses}>{this.formatCount()}</span>
-            <button className="btn btn-secondary btn-sm">Increment</button> 
-            <ul>
-        {this.state.tags.map(tag => <li key={tag}>{tag}</li>)}
-            </ul>
-            </div>
+         <div>
+             {this.state.tags.length === 0 && "Please create a new tag !!"}
+             {this.renderTags()}
+             </div>
+        
         )
 
-
     }
-    getBadgeClasses() {
-        const classes = "badge m-2 badge-";
-        classes += this.state.count === 0 ? "warning" : "primary";
-        return classes;
-    }
-
-        formatCount(){
-            const {count} = this.state;
-          return  count === 0 ? "Zero": count;
-        }
 
 }
  
