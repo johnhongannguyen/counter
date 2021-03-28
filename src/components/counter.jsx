45ebfc5c36e5c3de2c71603ac6a9475cf4913fb5
johnhongannguyen <johhaneshongan@gmail.com>
2021-03-27 23:21:19 -0700

    updating state

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -7,6 +7,9 @@ class Counter extends Component {
         tags: []
 
     };
+    handleIncrement = () =>{
+        this.setState({count: this.state.count + 1});
+    };
     renderTags(){
         if(this.state.tags.length === 0)
         return <p>There is no tag !!!</p>;
@@ -23,10 +26,20 @@ class Counter extends Component {
          <div>
              {this.state.tags.length === 0 && "Please create a new tag !!"}
              {this.renderTags()}
+             <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+             <button onClick={this.handleIncrement} className="btn btn-secondary btn-sm">Increment</button>
              </div>
         
         )
 
+    }
+    getBadgeClasses(){
+        let classes = "badge m-2 badge-";
+        classes += this.state.count === 0 ? "warning" : "primary";
+        return classes;
+    }
+    formatCount = () =>{
+        return this.state.count === 0 ? 'Zero' : this.state.count;
     }
 
 }
