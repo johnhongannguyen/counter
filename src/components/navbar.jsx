48193854982927a8056dd826d40ab00e19249384
johnhongannguyen <johhaneshongan@gmail.com>
2021-04-19 23:50:02 -0700

    multiple components in Sync

---
+++ src/components/navbar.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class NavBar extends Component {
+    state = {  }
+    render() { 
+        return ( 
+            <nav className="navbar navbar-light bg-light">
+                <a className="navbar-brand" href="#">This is a new app created by React.js</a>
+            </nav>
+         )
+    }
+}
+ 
+export default NavBar;
