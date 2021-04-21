d97c32f721588701afa02bcb42358dd7855b35cb
johnhongannguyen <johhaneshongan@gmail.com>
2021-04-20 23:50:29 -0700

    lifting the State Up

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -5,7 +5,12 @@ class NavBar extends Component {
     render() { 
         return ( 
             <nav className="navbar navbar-light bg-light">
-                <a className="navbar-brand" href="#">This is a new app created by React.js</a>
+                <a className="navbar-brand" href="#">
+                    Navbar{" "}
+                    <span className="badge badge-pill badge-secondary">
+                        {this.props.totalCounters}
+                    </span> 
+                </a>
             </nav>
          )
     }
